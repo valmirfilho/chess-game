d87adf23a1eff03cfab5927e2e41c81b94fe920d
valmirfilho <valmir@valmirsergio.com>
2019-01-17 11:22:23 -0200

    add class BoardException

---
+++ src/boadgame/BoardException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boadgame;
+
+/**
+ *
+ * @author root
+ */
+public class BoardException extends RuntimeException{
+    
+    private static final long serialVersionUID = 1L; 
+
+    public BoardException(String msg) {
+        super(msg);
+    }
+    
+
+    
+}
